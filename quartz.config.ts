ef67183dba04e97a5bd0f25da246250e655ffe51
succusana <scamantus@gmail.com>
2024-08-17 11:24:36 +0100

    Quartz sync: Aug 17, 2024, 11:24 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "en-GB",
     baseUrl: "ramble.alicealeph.co.uk",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
